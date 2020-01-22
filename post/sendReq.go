862d981ea7add02b7a2d4c0edca892ff5d3c8a6e
higashi000 <higashi.ict@gmail.com>
2020-01-22 22:26:32 +0900

    Add post source

---
+++ post/sendReq.go
@@ -0,0 +1,66 @@
+package post
+
+import (
+  "net/http"
+  "encoding/json"
+  "bytes"
+  "io/ioutil"
+  "fmt"
+  "log"
+)
+
+type Vimrc struct {
+  Body string `json:"body"`
+}
+
+type VimrcOption struct {
+  Indent int `json:"indent"`
+  ColorScheme string `json:"colorscheme"`
+}
+
+func generate(indent int, colorscheme string) {
+  url := "http://localhost:5000/generate"
+
+  orderVimrc := VimrcOption{}
+  orderVimrc.Indent = indent
+  orderVimrc.ColorScheme = colorscheme
+
+  sendData, err := json.Marshal(orderVimrc)
+  if err != nil {
+    log.Println("error struct to json")
+  }
+
+  req, err := http.NewRequest(
+    "POST",
+    url,
+    bytes.NewBuffer(sendData),
+  )
+  if err != nil {
+    log.Println("error req")
+    return
+  }
+
+  req.Header.Add("Content-Type", "application/json")
+
+  client := new(http.Client)
+  resp, err := client.Do(req)
+  if err != nil {
+    log.Println("error resp")
+    log.Println(err)
+    return
+  }
+  defer resp.Body.Close()
+
+  body, err := ioutil.ReadAll(resp.Body)
+
+  fmt.Println(string(body))
+
+  var vimrc Vimrc
+  err = json.Unmarshal(body, &vimrc)
+  if err != nil {
+    log.Println("error json")
+    return
+  }
+
+  fmt.Println(vimrc.Body)
+}
