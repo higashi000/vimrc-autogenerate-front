d4fd575916e495219285cf89a0c6c78326f60739
higashi000 <higashi.ict@gmail.com>
2020-01-29 18:13:27 +0900

    Add user name

--- makeVimrc/post/sendReq.go
+++ makeVimrc/post/sendReq.go
@@ -1,82 +1,83 @@
 package post
 
 import (
-  "net/http"
-  "encoding/json"
-  "bytes"
-  "io/ioutil"
-  "fmt"
-  "log"
-  "syscall/js"
-  "github.com/google/uuid"
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"syscall/js"
+
+	"github.com/google/uuid"
 )
 
 type Vimrc struct {
-  Body string `json:"body"`
+	Body string `json:"body"`
 }
 
 type VimrcOption struct {
-  Uuid string `json:"uuid"`
-  UserName string `json:"user"`
-  Indent int `json:"indent"`
-  ColorScheme string `json:"colorscheme"`
-  LangSetting []LanguageSettings `json:"langSettings"`
+	Uuid        string             `json:"uuid"`
+	UserName    string             `json:"user"`
+	Indent      int                `json:"indent"`
+	ColorScheme string             `json:"colorscheme"`
+	LangSetting []LanguageSettings `json:"langSettings"`
 }
 
 type LanguageSettings struct {
-  Language string `json:"language"`
-  Indent int `json:"indent"`
+	Language string `json:"language"`
+	Indent   int    `json:"indent"`
 }
 
 func Generate(orderVimrc VimrcOption) {
-  url := "http://localhost:5000/generate"
+	url := "http://localhost:5000/generate"
 
-  u, err := uuid.NewRandom()
-  if err != nil {
-    fmt.Println(err)
-    return
-  }
+	u, err := uuid.NewRandom()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 
-  orderVimrc.Uuid = u.String()
+	orderVimrc.Uuid = u.String()
 
-  document := js.Global().Get("document")
-  element := document.Call("getElementById", "uuid")
-  element.Set("value", orderVimrc.Uuid)
+	document := js.Global().Get("document")
+	element := document.Call("getElementById", "uuid")
+	element.Set("value", orderVimrc.Uuid)
 
-  sendData, err := json.Marshal(orderVimrc)
-  if err != nil {
-    log.Println("error struct to json")
-  }
+	sendData, err := json.Marshal(orderVimrc)
+	if err != nil {
+		log.Println("error struct to json")
+	}
 
-  req, err := http.NewRequest(
-    "POST",
-    url,
-    bytes.NewBuffer(sendData),
-  )
-  if err != nil {
-    log.Println("error req")
-    return
-  }
+	req, err := http.NewRequest(
+		"POST",
+		url,
+		bytes.NewBuffer(sendData),
+	)
+	if err != nil {
+		log.Println("error req")
+		return
+	}
 
-  req.Header.Add("Content-Type", "application/json")
+	req.Header.Add("Content-Type", "application/json")
 
-  client := new(http.Client)
-  resp, err := client.Do(req)
-  if err != nil {
-    log.Println("error resp")
-    log.Println(err)
-    return
-  }
-  defer resp.Body.Close()
+	client := new(http.Client)
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Println("error resp")
+		log.Println(err)
+		return
+	}
+	defer resp.Body.Close()
 
-  body, err := ioutil.ReadAll(resp.Body)
+	body, err := ioutil.ReadAll(resp.Body)
 
-  var vimrc Vimrc
-  err = json.Unmarshal(body, &vimrc)
-  if err != nil {
-    log.Println("error json")
-    return
-  }
+	var vimrc Vimrc
+	err = json.Unmarshal(body, &vimrc)
+	if err != nil {
+		log.Println("error json")
+		return
+	}
 
-  fmt.Println(vimrc.Body)
+	fmt.Println(vimrc.Body)
 }
--- result/main.go
+++ result/main.go
@@ -1,60 +1,62 @@
 package main
 
 import (
-  "net/http"
-  "encoding/json"
-  "io/ioutil"
-  "fmt"
-  "log"
-  "syscall/js"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"syscall/js"
 )
 
 type Vimrc struct {
-  Name string `json:"name"`
-  Body string `json:"body"`
+	Name string `json:"name"`
+	Body string `json:"body"`
 }
 
 func main() {
-  document := js.Global().Get("document")
-  vimrcUUID := document.Call("getElementById", "vimrcUUID")
-  url := "http://localhost:5000/result/" + vimrcUUID.Get("value").String()
+	document := js.Global().Get("document")
+	vimrcUUID := document.Call("getElementById", "vimrcUUID")
+	url := "http://localhost:5000/result/" + vimrcUUID.Get("value").String()
 
-  req, err := http.NewRequest(
-    "GET",
-    url,
-    nil,
-  )
-  if err != nil {
-    return
-  }
+	req, err := http.NewRequest(
+		"GET",
+		url,
+		nil,
+	)
+	if err != nil {
+		return
+	}
 
-  req.Header.Add("Content-Type", "application/json")
+	req.Header.Add("Content-Type", "application/json")
 
-  client := new(http.Client)
+	client := new(http.Client)
 
-  resp, err := client.Do(req)
-  if err != nil {
-    return
-  }
+	resp, err := client.Do(req)
+	if err != nil {
+		return
+	}
 
-  defer resp.Body.Close()
+	defer resp.Body.Close()
 
-  body, err := ioutil.ReadAll(resp.Body)
-  if err != nil {
-    return
-  }
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return
+	}
 
-  var vimrc Vimrc
+	var vimrc Vimrc
 
-  err = json.Unmarshal(body, &vimrc)
-  if err != nil {
-    log.Println("error json")
-    return
-  }
+	err = json.Unmarshal(body, &vimrc)
+	if err != nil {
+		log.Println("error json")
+		return
+	}
 
+	vimrcEle := document.Call("getElementById", "vimrc")
+	vimrcEle.Set("innerText", vimrc.Body)
 
-  vimrcEle := document.Call("getElementById", "vimrc")
-  vimrcEle.Set("innerText", vimrc.Body)
+	user := document.Call("getElementById", "user")
+	user.Set("innerText", "このvimrcを作ったのは"+vimrc.Name)
 
-  fmt.Println(vimrc.Body)
+	fmt.Println(vimrc.Body)
 }
