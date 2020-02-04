0acb871d1ef8a5e810652c0720e69ca21632b5ea
higashi000 <higashi.ict@gmail.com>
2020-02-04 17:59:56 +0900

    Add copy to clipboard

--- result/index.js
+++ result/index.js
@@ -7,3 +7,20 @@ const go = new Go();
 WebAssembly.instantiateStreaming(fetch("main.wasm"), go.importObject).then((result) => {
   go.run(result.instance);
 });
+
+function vimrcClipboard() {
+  let vimrcEle = document.createElement("textarea");
+  vimrcEle.value = document.getElementById("vimrc").innerText;
+
+  document.body.appendChild(vimrcEle);
+  vimrcEle.select();
+  let res = document.execCommand("copy")
+
+  vimrcEle.parentElement.removeChild(vimrcEle)
+
+  if (res === true) {
+    alert("クリップボードにコピーしました");
+  } else {
+    alert("クリップボードへのコピーに失敗しました");
+  }
+}
